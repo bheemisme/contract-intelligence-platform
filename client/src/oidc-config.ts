f12b8ee786becc59878c1f00214f1fc3b1e215ec
bheemisme <vemarapu.sudarshan@outlook.com>
2026-02-21 11:00:58 +0530

    add client deployment workflow

--- client/src/oidc-config.ts
+++ client/src/oidc-config.ts
@@ -4,13 +4,11 @@ export const googleOidcConfig: AuthProviderProps = {
   authority: 'https://accounts.google.com', // OpenID Connect provider (Google)
   client_id: import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID,
   redirect_uri: import.meta.env.VITE_GOOGLE_AUTH_REDIRECT_URI,
-  // post_logout_redirect_uri: import.meta.env.VITE_GOOGLE_AUTH_LOGOUT_REDIRECT_URI,
   client_secret: import.meta.env.VITE_GOOGLE_CLIENT_SECRET,
-  response_type: 'code', // use Authorization Code flow (PKCE)
+  response_type: "code", // use Authorization Code flow (PKCE)
   scope: 'openid profile email',
   automaticSilentRenew: false,
   loadUserInfo: false,
   filterProtocolClaims: true,
-
 };
 
